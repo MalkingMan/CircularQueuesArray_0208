7f3cb1982b1766a333df400ad04249db7d20e64c
MalkingMan <arrayalkhozini@gmail.com>
2024-05-30 14:47:45 +0700

    commit 3

--- CiricularQueues.cpp
+++ CiricularQueues.cpp
@@ -3,14 +3,29 @@ using namespace std;
 
 class Queues
 {
-private:
-    static const int max = 3;
-    int FRONT, REAR;
-    int queue_array{max};
+    private:
+        static const int max = 3;
+        int FRONT, REAR;
+        int queue_array{max};
 
-public:
-	queues() 
-    {
-		FRONT = -1;
-		REAR = -1;
-	}
+    public:
+        queues() 
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+
+    void insert()
+        {
+            int num;
+            cout << "Enter a number:";
+            cin >> num;
+            cout << endl;
+
+            // Cek apakah antrian penuh
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+        }
