5f91e0f8e890acd90694940db5006350b41b80e1
MalkingMan <arrayalkhozini@gmail.com>
2024-05-30 15:27:27 +0700

    commit 11

--- CiricularQueues.cpp
+++ CiricularQueues.cpp
@@ -92,9 +92,32 @@ class queues
         if (FRONT_Position <= REAR_Position)
         {
             while (FRONT_Position <= REAR_Position)
-            cout << queue_array[FRONT_Position] << "   ";
-				FRONT_Position++;
+            {
+                cout << queue_array[FRONT_Position] << "   ";
+			    FRONT_Position++;
+            }
+            cout << endl;
 		}
+
+        else
+        {
+            // jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+			while (FRONT_Position <= max - 1) 
+            {
+				cout << queue_array[FRONT_Position] << "   ";
+				FRONT_Position++;
+			}
+            
+
+
+
+        FRONT_Position = 0;
+
+        // Iterasi dari awal array hingga REAR
+        while (FRONT_Position <= REAR_Position)
+        {
+            cout 
+        }
         
     }
 
