9097509da8b0539179c6c67dea2c442f8f92cea5
MalkingMan <arrayalkhozini@gmail.com>
2024-05-30 15:40:05 +0700

    commit 15

--- CiricularQueues.cpp
+++ CiricularQueues.cpp
@@ -141,16 +141,27 @@ int main()
 
             switch (ch)
             {
-                case 1 :
+                case '1' :
                 {
                     q.insert();
                     break;
                 }
 
-                case 2 :
+                case '2' :
                 {
                     q.remove();
                     break;
+                }
+
+                case '3' :
+                {
+                    q.display();
+                    break;
+                }
+
+                case '4' :
+                {
+                    return 0;
                 }
 
             }
